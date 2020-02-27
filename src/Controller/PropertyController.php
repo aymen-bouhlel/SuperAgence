1e619d4e314d27d3e727789c867b1c2a3f3a9971
Bouhlel El Bechri <bouhlel_elbechri@web.de>
2020-02-27 14:34:40 +0100

    PropertyEntity + creation d'un bien dans le base de donnée

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -2,8 +2,9 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Property;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PropertyController extends AbstractController
 {
@@ -12,6 +13,23 @@ class PropertyController extends AbstractController
      */
     public function index()
     {
+        $property = new Property();
+        $property->setTitle("Mon premier Bien");
+        $property->setRooms(2);
+        $property->setPrice(20000);
+        $property->setBedrooms(2);
+        $property->setSurface(60);
+        $property->setDescription("Une petite description");
+        $property->setFloor(4);
+        $property->setHeat(1);
+        $property->setCity("Montpelier");
+        $property->setAdresse("15 Boulevard Gambetta");
+        $property->setPostaleCode(34000);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($property);
+        $em->flush();
+        
         return $this->render('property/index.html.twig', [
             'current_menu' => 'Properties',
         ]);
