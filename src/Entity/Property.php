9298a6bd6c3525344bd7ebe6cb01e1c653bc6d76
Bouhlel El Bechri <bouhlel_elbechri@web.de>
2020-03-03 09:14:16 +0100

    Valider les données: Title unique, surface entre 10 et 400 m

--- src/Entity/Property.php
+++ src/Entity/Property.php
@@ -4,9 +4,12 @@ namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Cocur\Slugify\Slugify;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\PropertyRepository")
+ * @UniqueEntity("title")
  */
 class Property
 {
@@ -25,6 +28,12 @@ class Property
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Length(
+     *      min = 5,
+     *      max = 255,
+     *      minMessage = "Title  must be at least 5 characters long",
+     *      maxMessage = "Title  cannot be longer than 255 characters"
+     * )
      */
     private $title;
 
@@ -35,6 +44,12 @@ class Property
 
     /**
      * @ORM\Column(type="integer")
+     * @Assert\Range(
+     *      min = 10,
+     *      max = 400,
+     *      minMessage = "Surface must be at least 10 m² tall to enter",
+     *      maxMessage = "Surface cannot be taller than 400 m² to enter"
+     * )
      */
     private $surface;
 
@@ -75,6 +90,7 @@ class Property
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Regex("/^[0-9]{5}$/")
      */
     private $postale_code;
 
